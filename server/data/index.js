ede8f33274830dad7ff38171b7a3b005f48d657a
vaibhav-0310 <vbhargav0310@gmail.com>
2025-07-09 00:07:44 +0530

    production url

--- server/data/index.js
+++ server/data/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Blogs from '../schema/blogs.schema.js';
-import Blog from './blog.js'; 
+import Vet from '../schema/vet.schemas.js';
+import data from './vet.js'; 
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,7 +9,7 @@ const app=express();
 
 const connect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/pawvaidya");
+    await mongoose.connect("mongodb+srv://vbhargav0310:Jaimatadi@cluster0.yo8jy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/pawvaidya");
     console.log("Connected to database");
   } catch (error) {
     console.error("Error connecting to database:", error);
@@ -17,8 +17,8 @@ const connect = async () => {
 };
 const pushEssentials = async (essentialsArray) => {
   try {
-    await Blogs.deleteMany({}); 
-    await Blogs.insertMany(essentialsArray);
+    await Vet.deleteMany({}); 
+    await Vet.insertMany(essentialsArray);
     console.log("All essential items saved successfully");
   } catch (error) {
     console.error("Error saving essential items:", error);
@@ -26,7 +26,7 @@ const pushEssentials = async (essentialsArray) => {
 };
 
 connect()
-  .then(() => pushEssentials(Blog))
+  .then(() => pushEssentials(data))
   .catch((err) => {
     console.error("Failed to push essentials:", err);
   });
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -115,7 +115,7 @@ router.get("/auth/google", passport.authenticate("google", { scope: ["profile", 
 router.get(
   "/auth/google/callback",
   passport.authenticate('google', { 
-    failureRedirect: 'http://localhost:8080/login',
+    failureRedirect: 'https://pawvaidya-vbdx.onrender.com/login',
     session: true, 
   }),
   async (req, res) => {
@@ -137,13 +137,13 @@ router.get(
       req.login(user, (err) => {
         if (err) {
           console.error("Login error:", err);
-          return res.redirect('http://localhost:8080/login');
+          return res.redirect('https://pawvaidya-vbdx.onrender.com/login');
         }
-        res.redirect('http://localhost:8080');
+        res.redirect('https://pawvaidya-vbdx.onrender.com');
       });
     } catch (error) {
       console.error("Google callback error:", error);
-      res.redirect('http://localhost:8080/login');
+      res.redirect('https://pawvaidya-vbdx.onrender.com/login');
     }
   }
 );
