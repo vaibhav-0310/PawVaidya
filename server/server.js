cac60cca4e083222e6dd6315a46166150a27218d
vaibhav-0310 <vbhargav0310@gmail.com>
2025-07-14 17:08:25 +0530

    reintialize development phase

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENTID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://pawvaidya-vbdx.onrender.com/api/auth/google/callback",
+    callbackURL: "http://localhost:8080/api/auth/google/callback",
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
